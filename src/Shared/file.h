86ad687564ca112f1559ebf7ee36985b31870418
TIR-ALMA <visotskayoa@mail.ru>
2026-02-08 22:54:41 +0700

    Update file.h

--- src/Shared/file.h
+++ src/Shared/file.h
@@ -1,8 +1,9 @@
 #ifndef __FILE_H__
 #define __FILE_H__
 
-#include <windows.h>
+#include <Windows.h>
 
+BOOL FileGetInfo(HANDLE FileHandle, struct _FILE_STANDARD_INFORMATION* Info);
 BOOL FileGetSize(HANDLE FileHandle, PDWORD FileSize);
 BOOL FileOpen(HANDLE* FileHandle, LPCWSTR Path, ACCESS_MASK AccessMask, ULONG CreateDisposition);
 BOOL FileWrite(HANDLE FileHandle, LPCVOID Buffer, DWORD Length);
@@ -12,6 +13,7 @@ BOOL FileReadBuffer(LPCWSTR Path, LPVOID* Buffer, PDWORD Length);
 BOOL FileCreateDirectory(LPCWSTR Path);
 BOOL FileDelete(LPCWSTR Path);
 BOOL FileCopy(LPCWSTR OriginalPath, LPCWSTR NewPath, BOOL DeleteOriginal);
+BOOL IsValidNtPath(LPCWSTR Path);
 BOOL DosPathToNtPath(LPWSTR* Path);
 
 #endif

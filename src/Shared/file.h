527e722675df6c5e761d1967407d2fe0dfeb2b2f
TIR-ALMA <visotskayoa@mail.ru>
2026-02-08 22:14:28 +0700

    Update file.h

--- src/Shared/file.h
+++ src/Shared/file.h
@@ -3,11 +3,10 @@
 
 #include <windows.h>
 
-BOOL FileGetInfo(HANDLE FileHandle, PFILE_STANDARD_INFORMATION Info);
-BOOL FileGetSize(HANDLE FileHandle, PLARGE_INTEGER FileSize);
+BOOL FileGetSize(HANDLE FileHandle, PDWORD FileSize);
 BOOL FileOpen(HANDLE* FileHandle, LPCWSTR Path, ACCESS_MASK AccessMask, ULONG CreateDisposition);
-BOOL FileWrite(HANDLE FileHandle, LPCVOID Buffer, DWORD Length, PDWORD BytesWritten);
-BOOL FileRead(HANDLE FileHandle, LPVOID Buffer, DWORD Length, PDWORD BytesRead);
+BOOL FileWrite(HANDLE FileHandle, LPCVOID Buffer, DWORD Length);
+BOOL FileRead(HANDLE FileHandle, LPVOID* Buffer, DWORD Length, PDWORD ReadLength);
 BOOL FileWriteBuffer(LPCWSTR Path, LPCVOID Buffer, DWORD Length, BOOL Append);
 BOOL FileReadBuffer(LPCWSTR Path, LPVOID* Buffer, PDWORD Length);
 BOOL FileCreateDirectory(LPCWSTR Path);

6b22537578ee3c173395df788a4ec834e5aeab00
TIR-ALMA <visotskayoa@mail.ru>
2026-02-08 22:08:32 +0700

    Update file.h

--- src/Shared/file.h
+++ src/Shared/file.h
@@ -1,16 +1,19 @@
 #ifndef __FILE_H__
 #define __FILE_H__
 
+#include <windows.h>
+
 BOOL FileGetInfo(HANDLE FileHandle, PFILE_STANDARD_INFORMATION Info);
-BOOL FileGetSize(HANDLE FileHandle, PDWORD FileSize);
-BOOL FileOpen(HANDLE* FileHandle, CONST LPWSTR Path, ACCESS_MASK AccessMask, ULONG CreateDisposition);
-BOOL FileWrite(HANDLE FileHandle, CONST LPVOID Buffer, DWORD Length);
-BOOL FileRead(HANDLE FileHandle, LPVOID* Buffer, DWORD Length, PDWORD ReadLength);
-BOOL FileWriteBuffer(CONST LPWSTR Path, CONST LPVOID Buffer, DWORD Length, BOOL Append);
-BOOL FileReadBuffer(CONST LPWSTR Path, LPVOID* Buffer, PDWORD Length);
-BOOL FileCreateDirectory(const LPWSTR Path);
-BOOL FileDelete(CONST LPWSTR Path);
-BOOL FileCopy(CONST LPWSTR OriginalPath, CONST LPWSTR NewPath, BOOL DeleteOriginal);
+BOOL FileGetSize(HANDLE FileHandle, PLARGE_INTEGER FileSize);
+BOOL FileOpen(HANDLE* FileHandle, LPCWSTR Path, ACCESS_MASK AccessMask, ULONG CreateDisposition);
+BOOL FileWrite(HANDLE FileHandle, LPCVOID Buffer, DWORD Length, PDWORD BytesWritten);
+BOOL FileRead(HANDLE FileHandle, LPVOID Buffer, DWORD Length, PDWORD BytesRead);
+BOOL FileWriteBuffer(LPCWSTR Path, LPCVOID Buffer, DWORD Length, BOOL Append);
+BOOL FileReadBuffer(LPCWSTR Path, LPVOID* Buffer, PDWORD Length);
+BOOL FileCreateDirectory(LPCWSTR Path);
+BOOL FileDelete(LPCWSTR Path);
+BOOL FileCopy(LPCWSTR OriginalPath, LPCWSTR NewPath, BOOL DeleteOriginal);
 BOOL DosPathToNtPath(LPWSTR* Path);
 
 #endif
+

